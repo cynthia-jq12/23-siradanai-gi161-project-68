e0f67acaa32ae31ab5391dc8fd6822ad1bde1d93
cynthia-jq12 <cxnthiazen12@gmail.com>
2025-11-30 22:49:30 +0700

    Remove player, enemy, weapon, and UI scripts
    Deleted core gameplay scripts including Player, Enemy, Weapon, and related projectile and controller scripts, as well as UI components like HealthBar and GameManager. Also refactored IShootable from an interface to a MonoBehaviour class with empty Start and Update methods. This change likely prepares for a major refactor or migration of the game's architecture.

--- Assets/Scripts/IShootable.cs
+++ Assets/Scripts/IShootable.cs
@@ -1,8 +1,16 @@
 using UnityEngine;
 
-public interface IShootable
+public class IShootable : MonoBehaviour
 {
-    GameObject BulletPrefab { get; set; }
-    Transform ShootPoint { get; set; }
-    void Shoot();
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
 }
